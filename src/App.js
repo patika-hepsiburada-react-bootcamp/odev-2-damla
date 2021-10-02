cc6d6836b201dd738eadfbd260884e817c5dbd5e
Damla Koksal <me@damlakoksal.com>
2021-10-02 21:24:13 +0300

    test: changes done to add dev branch

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="hangman-logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          test
+          Test
         </a>
       </header>
     </div>
