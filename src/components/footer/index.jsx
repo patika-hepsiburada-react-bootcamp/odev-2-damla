52391ccc39996623011adb721278fed9f4657280
Damla Koksal <me@damlakoksal.com>
2021-10-03 03:59:08 +0300

    [#1] feat: create footer component

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.scss'
+
+
+function Footer() {
+    return (
+        <div className={styles.container}>
+            Made with ❤️ by&nbsp;<a href="https://github.com/damla" rel="noopener noreferrer" target="_blank">damla</a>
+        </div>
+    )
+}
+
+export default Footer;
